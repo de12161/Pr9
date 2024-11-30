5bcf70160efa371aa29d8e596a92cc9e94d0110b
de12161 <162108835+de12161@users.noreply.github.com>
2024-11-30 15:26:59 +0300

    Update Problem2.kt

--- src/main/kotlin/Problem2.kt
+++ src/main/kotlin/Problem2.kt
@@ -2,16 +2,10 @@ internal object Problem2 : ISolution {
     override val name: String
         get() = "Задача 2"
 
-    private fun intersection(l1: IntArray, l2: IntArray): Set<Int> {
-        val ret = mutableSetOf<Int>()
-        l1.forEach { if (l2.contains(it)) ret.add(it) }
-        return ret
-    }
-
     override fun solve(): String {
         val nums1 = intArrayOf(1, 2, 2, 1)
         val nums2 = intArrayOf(2, 2)
 
-        return intersection(nums1, nums2).toString()
+        return nums1.filter { nums2.contains(it) }.associateWith { 0 }.keys.toString()
     }
 }
