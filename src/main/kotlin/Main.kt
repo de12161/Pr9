9a63cb78aee0e80cd4895765f200f0fbb69d1263
de12161 <162108835+de12161@users.noreply.github.com>
2024-11-30 14:33:22 +0300

    Solve problem 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,8 @@ fun main() {
     ProblemList.addProblems(
         Problem1,
         Problem2,
-        Problem3
+        Problem3,
+        Problem4
     )
 
     ProblemList.printResults()
---
+++ src/main/kotlin/Problem4.kt
@@ -0,0 +1,11 @@
+internal object Problem4 : ISolution {
+    override val name: String
+        get() = "Задача 4"
+
+    override fun solve(): String {
+        val nums = arrayListOf(8, 1, 2, 2, 3)
+
+        var i = 0
+        return nums.associate { num -> Pair(i++, nums.count { it < num }) }.values.toString()
+    }
+}
